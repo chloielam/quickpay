c78c26cf97842323a85c73a96c8465b1ec53998f
Gia Truc Lam <truc.lamg@gmail.com>
2026-06-03 16:26:32 -0230

    Fix bug of removing error

--- app/users/users_routes.py
+++ app/users/users_routes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, status
 
 from .users_schemas import UserCreate, UserResponse
 from .users_services import create_user
-from app.core.errors import ERR_AGE_RESTRICTED, ERR_INVALID_EMAIL, ERR_DUPLICATE_USER
+from app.core.errors import ERR_AGE_RESTRICTED, ERR_INVALID_EMAIL
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -13,6 +13,6 @@ def create_new_user(user_data: UserCreate):
         return create_user(user_data)
     except ValueError as e:
         msg = str(e)
-        if msg in {ERR_AGE_RESTRICTED, ERR_INVALID_EMAIL, ERR_DUPLICATE_USER}:
+        if msg in {ERR_AGE_RESTRICTED, ERR_INVALID_EMAIL}:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=msg)
--- app/users/users_services.py
+++ app/users/users_services.py
@@ -1,15 +1,11 @@
 from .users_schemas import UserCreate, UserResponse
 from .users_validators import validate_age, validate_email
-from app.core.errors import ERR_DUPLICATE_USER
 from app.users.users_repository import get_user_by_email, save_user
 
 
 def create_user(user_data: UserCreate) -> UserResponse:
     validate_age(user_data.age)
     validate_email(user_data.email)
-
-    if get_user_by_email(user_data.email) is not None:
-        raise ValueError(ERR_DUPLICATE_USER)
 
     return save_user(user_data)
 
